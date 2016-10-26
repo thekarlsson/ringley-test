fcf597a5aa35102cb6f5309c17da55fd46c5fefa
Joachim Karlsson <karlson.joachim@gmail.com>
2016-10-26 13:39:31 +0100

    Recognizes last day of month

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ My solution to your technical test.
 
 Known problems:
 
-Does not consider the last day of a month to always equal the last day of another month.
+Doesn't handle arbitrary years.
--- reminder.rb
+++ reminder.rb
@@ -7,14 +7,28 @@ class Reminder
     estates_in_need_of_reminding = []
 
     estates.each do |estate|
-      due_date_offset = date.next_month(@rules[estate.charge_period.to_sym])
+      due_date_offset = offset_date(date, @rules[estate.charge_period.to_sym])
       next_due_date = estate.next_due_date(date)
 
-      if due_date_offset >= next_due_date && date <= next_due_date
+      if due_date_offset >= next_due_date
         estates_in_need_of_reminding << "#{estate.code} due date #{next_due_date}"
       end
     end
-    
+
     return estates_in_need_of_reminding
+  end
+
+  private
+
+  def last_day_of_month?(date)
+    date.next_day.day == 1
+  end
+
+  def offset_date(date, offset)
+    unless last_day_of_month?(date)
+      date.next_month(offset)
+    else
+      date.next_day.next_month(offset).prev_day
+    end
   end
 end
